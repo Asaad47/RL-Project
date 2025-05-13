721e84678713fdd99b23c068c669411f1fe18b4e
Asaad47 <75583625+Asaad47@users.noreply.github.com>
2025-05-13 23:02:14 +0300

    Add experiments scripts

---
+++ src/run_simple.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+tracks=("zengarden" "lighthouse" "hacienda" "snowtuxpeak" "cornfield_crossing" "scotland")
+
+for track in "${tracks[@]}"; do
+    python simple_RL_controller.py --track $track --mode train --max_frames 2000 --episodes 3000
+done
+
+for track in "${tracks[@]}"; do
+    python simple_RL_controller.py --track $track --mode test --q_table_path trained_models/$track/simple_qtable_ep1000.npy
+    python simple_RL_controller.py --track $track --mode test --q_table_path trained_models/$track/simple_qtable_ep2000.npy
+    python simple_RL_controller.py --track $track --mode test --q_table_path trained_models/$track/simple_qtable_ep3000.npy
+done
